a1b5a1c12697a2d081239165ef613b97f827bf19
”Ismail” <ibeno95@mail.ru>
2022-09-14 14:36:20 +0300

    сделал добавление и удаление из избранного

--- routes/users.route.js
+++ routes/users.route.js
@@ -7,5 +7,7 @@ const router = Router();
 router.post("/users/auth", usersController.addUser);
 router.post("/users/login", usersController.login);
 router.get("/users/user", Middleware, usersController.getUsersid);
+router.patch('/users/addFav/:id', Middleware, usersController.addFavorite)
+router.patch('/users/removeFav/:id', Middleware, usersController.removeFavorite)
 
 module.exports = router;
