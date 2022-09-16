b13152cb56a37147bc00a9e2cdb05abcaaa0cebf
OdinTakoy <bugazov9@mail.ru>
2022-09-16 21:12:26 +0300

    fdf

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
 const cors = require("cors");
+const morgan = require("morgan")
 require("dotenv").config();
+app.use(morgan("dev"))
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.resolve(__dirname, "public")));
